7dd485e35a32fd1d07a544bc0f07718f02c84c9b
tastulae <tastulae@mail.usf.edu>
2014-09-04 02:29:35 +0000

    get, sweetieException, hasIndex and map functions

--- collection.js
+++ collection.js
@@ -11,7 +11,46 @@ module.exports = function(internalArray,Collection){
             var independentInternalArray = this.toArray();
             var newIndependentInstance = Collection(independentInternalArray); 
             return newIndependentInstance;
+        }, //<------yay I did it!!
+        get:function(indx){
+            if (indx < internalArray.length){
+               return internalArray[indx];
+            }
+            else
+            { 
+                throw new sweetieException(indx);  
+            }
+        },
+        sweetieException:function(indx) {
+            this.indx = indx;
+            this.message1 = "This subscript";
+            this.message2 = "is out of range, sweetie!!";
+            this.toString = function() {
+            return this.message1 + this.indx + this.message2;
+            }
+        },
+        hasIndex:function(indx){
+           if (indx < internalArray.length){
+               return true;
+            }
+            else
+            { 
+                return false;  
+            }  
+        },
+        map:function(fnct){
+            var mappedArray = [];
+            var j = 0;
+            for (; j < internalArray.length; j++){
+               mappedArray[j]=fnct(internalArray[j]);
+               }
+            var newInstance = Collection(mappedArray); 
+            return newInstance;    
         }
-      
+        
+        //also, i just pushed the latest to github so we have a recent backup in version control
+        //you can do this too by going to the terminal:
+        //Window -> New Terminal -> git add -A (adds changes to working space) -> git commit -m "commit message here" -> 
+        //git push origin master (push latest commits to remote repository on github.com)
     }
 }
